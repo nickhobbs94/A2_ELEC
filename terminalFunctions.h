04e9a24b561cc8538b6a5a4a21562af739bb61d3
Roxy <roxanne.jackson@uon.edu.au>
2015-04-07 08:18:51 +1000

    Added ledr function without LED display on DE2 board

--- terminalFunctions.h
+++ terminalFunctions.h
@@ -1,11 +1,16 @@
 #ifndef TERMINALFUNCTIONS_H
 #define TERMINALFUNCTIONS_H
 #include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
 #include "conversions.h"
+#define NUMBER_OF_LEDS 18
+#define BINARY_BITS_IN_DECIMAL(decimalNumber) log2(decimalNumber)+1
 
 /* Function prototypes */
 int echo(int argc, char* argv[]);
 int add(int argc, char* argv[]);
+int ledr(int argc, char* argv[]);
 
 /* Functions */
 int echo(int argc, char* argv[]){
@@ -21,7 +26,7 @@ int add(int argc, char* argv[]){
     int sum=0;
     int i;
     int temp;
-    for (int i=1; i<argc; i++){
+    for (i=1; i<argc; i++){
         temp = intfromstring(argv[i]);
         if (temp == -1){
             return -1;
@@ -32,6 +37,26 @@ int add(int argc, char* argv[]){
     
     
     printf("%d\n",sum);
+    return 0;
+}
+
+int ledr(int argc, char* argv[]){
+    int i, j;
+    int dec;
+    for (i=1; i<argc; i++){
+        dec = intfromstring(argv[i]);
+        if (dec == -1){
+            return -1;
+        }     
+    }
+    int* binary = calloc(BINARY_BITS_IN_DECIMAL(dec),sizeof(bool));
+    decimaltobinary(binary, dec);
+    //Enter stuff in here to display on red LEDs on dev board
+    for(j=BINARY_BITS_IN_DECIMAL(dec)-1; j>=0; --j){
+        printf(binary[j]?"1":"0");
+    }
+    printf("\n");
+    free(binary);
     return 0;
 }
 
